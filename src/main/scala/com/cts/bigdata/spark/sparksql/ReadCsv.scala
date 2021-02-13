b8c89543f6126cb66380e6be0a7e287148ac0cd9
RAJ <raj.te027@yahoo.com>
2021-02-13 08:43:31 +0530

    SelfLearning

---
+++ src/main/scala/com/cts/bigdata/spark/sparksql/ReadCsv.scala
@@ -0,0 +1,24 @@
+package com.cts.bigdata.spark.sparksql
+
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
+
+object ReadCsv {
+  def main(args: Array[String]): Unit = {
+
+    val spark = SparkSession.builder.master("local[*]").appName("myaname").getOrCreate()
+    val df =
+    //spark.read
+    // .option("header", "true")
+    // .csv("F:\\Documents\\PLSQL Programs\\Hadoop\\Dataset\\us-500.csv")
+
+      spark.read
+        .option("header", "true")
+        .format("csv").load("F:\\Documents\\PLSQL Programs\\Hadoop\\Dataset\\us-500.csv")
+
+    df.printSchema()
+    df.show()
+
+  }
+
+}
