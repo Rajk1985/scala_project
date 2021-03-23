0f87e053c71bbf71d68abe17f026370c695bd0ac
RAJ <raj.te027@yahoo.com>
2021-03-23 16:04:17 +0530

    getOracleData

--- src/main/scala/com/cts/bigdata/spark/sparksql/getOracleData.scala
+++ src/main/scala/com/cts/bigdata/spark/sparksql/getOracleData.scala
@@ -13,32 +13,33 @@ object getOracleData {
     import spark.sql
     //----------Write Logic Here--------------------------
     val url ="jdbc:oracle:thin://localhost:1521/orcl"
-    //val df = spark.read.format("jdbc").option("url",url).option("user","scott")
-       //      .option("password","tiger").option("driver","oracle.jdbc.OracleDriver").option("dbtable","DEPT").load()
-    //df.show()
+    val df = spark.read.format("jdbc").option("url",url).option("user","scott")
+            .option("password","tiger").option("driver","oracle.jdbc.OracleDriver").option("dbtable","DEPT").load()
+    df.show()
     //if u get class not found exception oracle.jdbc.OracleDriver ... go to file> project structure >> dependencies
-    //    //+  add ojdbc7.jar ... ok
+    //  +  add ojdbc7.jar ... ok
 
-   // val qry = "Select * from dept where deptno = 10;"
-   // val df = spark.read.format("jdbc").option("url",url).option("user","scott")
-   //   .option("password","tiger").option("driver","oracle.jdbc.OracleDriver").option("dbtable","qry").load()
-   // df.show()
+    //************using SQL queries***************************
 
-    /**Writing data frm CSV file to Database ***/
+    val qry = "(Select * from dept where deptno = 10)"
+    val df1 = spark.read.format("jdbc").option("url",url).option("user","scott")
+     .option("password","tiger").option("driver","oracle.jdbc.OracleDriver").option("dbtable",qry).load()
+    df1.show()
+
+    /**********Writing data frm CSV file to Database *********/
 
     val data = "F:\\bigdata\\Dataset\\10000Records.csv"
-    val df = spark.read.format("csv").option("header","true").option("inferSchema","true").load(data)
-     val reg = "[^a-zA-Z0-9]"
-     val cols = df.columns.map(x=>x.replaceAll(reg,""))
+    val df2 = spark.read.format("csv").option("header","true").option("inferSchema","true").load(data)
+    val reg = "[^a-zA-Z0-9]"
+    val cols = df2.columns.map(x=>x.replaceAll(reg,""))
 
     //To put date in specific format use Format option
-    val ndf = df.toDF(cols:_*).withColumn("DateofBirth",to_date($"DateofBirth","MM/dd/yyyy"))
+    val ndf = df2.toDF(cols:_*).withColumn("DateofBirth",to_date($"DateofBirth","MM/dd/yyyy"))
       .withColumn("DateofJoining",to_date($"DateofJoining","MM/dd/yyyy"))
 
     //Write records to database
     ndf.write.mode(SaveMode.Overwrite).format("jdbc").option("url",url).option("user","scott").option("password","tiger")
       .option("driver","oracle.jdbc.OracleDriver").option("dbtable","my10krecords").save()
-
 
     //---------------------------------------------------
     spark.stop()
